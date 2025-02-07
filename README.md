0f48f941c1c82c16d3a97c76ac51a43c5567a0f6
Sven Schrodt <sven@schrodt.nrw>
2025-02-07 14:34:53 +0100

    Mini changes

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # chatbot
 Projektarbeit Python 2025-I
+
+
+
+## Appendix
+
+## Beispiel-Dialog
+
+```mermaid
+
+sequenceDiagram
+    Bot ->> User: Hallo User, wie geht es Dir?
+    Note right of User: Beispieldialog
+    User--x Bot: Ach es geht so ...
+    Bot ->> User: Kann ich Dir helfen, das zu ändern?
+    User-->Bot: Ach, ich weiß es nicht..
+    
+    
+```
--- bot.py
+++ bot.py
@@ -1,10 +1,61 @@
 
-welcome = 'Guten Tag'
-answer_bot = 'Dummyantwort'
-quit = 'stop'
-question = 'Und weiter?'
-answer =''
-print(welcome)
-while answer.lower() != quit: 
-    answer = input(question)
-    print(answer_bot)
+class ChatBot:
+    pass # Platzhalter; "tue nix" --> um korrekte Syntax einzuhalten
+
+    bot_name = ''
+    language = 'DE' # Datenattribut
+    welcome = 'Guten Tag, ich heisse'
+    your_name = 'Und wie heisst Du?'
+    answer_bot = 'Dummyantwort'
+    quit = 'stop'
+    question = 'Und weiter? '
+    answer = ''
+
+    #####
+
+    known_names = ['Nadja', 'Andrei', 'Manuel', 'Sven', 'Jakob']
+
+    #####
+
+    user_name = ''
+    # Attribut -> Methoden / Funktionen
+
+    # __NAME__ --> double underscore --> 
+    def __init__(self, name):
+        self.bot_name = name
+
+
+    def run(self):
+        print(self.welcome, self.bot_name, self.your_name)
+     
+        while self.answer.lower() != self.quit:
+            self.answer = input(self.question)
+            self.split_words(self.answer)
+            self.bot_says()
+
+    def bot_says(self):
+        print(self.answer_bot)
+
+    def split_words(self, sentence: str): 
+        # Ich heisse nämlich Sven -> ['ich',  'heisse', 'Sven', 'nämlich' ]
+        tmp = sentence.split()
+        self.clean(tmp)
+        self.is_known(tmp)
+        print(tmp)
+        exit()
+
+    def clean(self, words: list):
+        for word in words:
+            #print(word)
+            pass
+    
+    def is_known(self, words: list):
+        for word in words:
+            if word in self.known_names:
+                self.user_name = word
+       
+
+
+Maja = ChatBot('Bine')
+
+Maja.run()
